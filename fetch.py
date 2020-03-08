b248b876ce7e3cce8cab4d4e1537f1a3364d1142
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-03-08 16:55:51 -0500

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -2,12 +2,17 @@ import urllib.request
 import lxml as lh
 
 
+#pulls URL, length 1 only, length 2 broken (look into later)
 url = "http://readpoopfiction.com/story.php?length=1"
+#open url
 page = urllib.request.urlopen(url)
+#read page
 doc = str(page.read())
+#format author section
 index = doc.find("<p class=\"author\">")
 doc = doc[index:-1]
 index = doc.find("<p>")
 doc = doc[index+3:-1]
 end = doc.find("<p class=\"more\">")
+#paragraph settings
 print(doc[:end].replace("<br />", "\n").replace("</p>", "").replace("\\t", "").replace("\\n","").replace("\\r", "").replace("<p>", "\n").replace("&nbsp;"," ").replace("&ldquo;","\"").replace("&rdquo;", "\""))
